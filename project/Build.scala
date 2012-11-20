b90042f1e4eb4536ba48035436fc8a774575a127
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2012-11-20 23:45:52 +0100

    Migrate to Play 2.1. Freeze with netty dependency with Finagle

--- project/Build.scala
+++ project/Build.scala
@@ -1,6 +1,6 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play.Project._
 
 object ApplicationBuild extends Build {
 
@@ -8,11 +8,13 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "com.twitter" % "finagle-core" % "5.3.22",
-      "com.twitter" % "finagle-http" % "5.3.22"
+        "com.twitter" % "finagle-core" % "5.3.22",
+        "com.twitter" % "finagle-http" % "5.3.22",
+        jdbc,
+        anorm
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+    val main = play.Project(appName, appVersion, appDependencies).settings(
       // Add your own project settings here      
     )
 
