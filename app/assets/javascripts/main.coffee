e3e2a4a40fd642394ce24cd2090aaf5357a066da
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2012-12-11 22:09:42 +0100

    localStorage prefs links

--- app/assets/javascripts/main.coffee
+++ app/assets/javascripts/main.coffee
@@ -1,6 +1,31 @@
 $(document).ready ->
+  getUrl = (prefix, suffix) ->
+    env = "all"
+    env = localStorage["environmentSelect"]  if localStorage["environmentSelect"]?
+    soapaction = "all"
+    soapaction = localStorage["soapActionSelect"]  if localStorage["soapActionSelect"]?
+    from = "all"
+    from = localStorage["from"]  if localStorage["from"]?
+    to = "today"
+    if localStorage["to"]?
+      to = localStorage["to"]
+      mDate = new Date()
+      month = mDate.getMonth() + 1
+      day = mDate.getDate()
+      month = "0" + month  if month < 10
+      day = "0" + day  if day < 10
+      sDate = mDate.getFullYear() + "-" + month + "-" + day
+      console.log "Date:" + sDate
+      to = "today"  if sDate is to
+      "/" + prefix + "/" + env + "/" + soapaction + "/" + from + "/" + to + "/" + suffix
+
   window.App = {}
   window.App.totalMemory = 400
+  $("#menu-search").click ->
+    document.location.href = getUrl("search", "")
+
+  $("#menu-analysis").click ->
+    document.location.href = getUrl("analysis", "200/")
 
   $("#toggle-monitoring").click ->
     init window.App, $("#monitoring-modal")
--- public/javascripts/soapower/storage.js
+++ public/javascripts/soapower/storage.js
@@ -5,34 +5,3 @@ function storeLocalStorage() {
     localStorage['from'] = $('#from').val();
     localStorage['to'] = $('#to').val();
 }
-
-/*function retrieveLocalStorage() {
-    var change = false;
-    if (localStorage['environmentSelect'] != null &&
-        localStorage['environmentSelect'] != $('#environmentSelect').val()) {
-        $('#environmentSelect').val(localStorage['environmentSelect']);
-        change = true;
-    }
-    if (localStorage['soapActionSelect'] != null &&
-        localStorage['soapActionSelect'] != $('#soapActionSelect').val()) {
-        $('#soapActionSelect').val(localStorage['soapActionSelect']);
-        change = true;
-    }
-    if (localStorage['statusSelect'] != null &&
-        localStorage['statusSelect'] != $('#statusSelect').val()) {
-        $('#statusSelect').val(localStorage['statusSelect']);
-        change = true;
-    }
-    if (localStorage['from'] != null &&
-        localStorage['from'] != $('#from').val()) {
-        $('#from').val(localStorage['from']);
-        change = true;
-    }
-    if (localStorage['to'] != null &&
-        localStorage['to'] != $('#to').val()) {
-        $('#to').val(localStorage['to']);
-        change = true;
-    }
-    if (change) document.location.href=makeUrl();
-}
-*/
