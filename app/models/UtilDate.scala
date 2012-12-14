8d976309da55e07172295bfba55bf4aed2902719
Guillaume Tinon <guillaume.tinon@gmail.com>
2012-12-14 16:02:52 +0100

    Support 'today-XX' in URL for dates

--- app/models/UtilDate.scala
+++ app/models/UtilDate.scala
@@ -1,21 +1,22 @@
 package models
 
-import java.util.{Calendar, Date, GregorianCalendar}
+import java.util.{ Calendar, Date, GregorianCalendar }
 import java.text.SimpleDateFormat
-
 
 object UtilDate {
 
-  val v23h59min59s = ((24*60*60)-1)*1000
-  val v1d = 24*60*60*1000
+  val v23h59min59s = ((24 * 60 * 60) - 1) * 1000
+  val v1d = 24 * 60 * 60 * 1000
+  val pattern = "today-([0-9]+)".r
 
-  def getDate(sDate: String, addInMillis : Long = 0):GregorianCalendar = {
+  def getDate(sDate: String, addInMillis: Long = 0): GregorianCalendar = {
     val gCal = new GregorianCalendar()
 
-    val mDate : GregorianCalendar = sDate match {
+    val mDate: GregorianCalendar = sDate match {
       case "all" => gCal.setTime(RequestData.getMinStartTime.getOrElse(new Date)); gCal
       case "today" => gCal
-      case "yesterday" => gCal.add( Calendar.DATE, -1 ); gCal
+      case "yesterday" => gCal.add(Calendar.DATE, -1); gCal
+      case pattern(days) => gCal.add(Calendar.DATE, -(days.toInt)); gCal
       case _ =>
         val f = new SimpleDateFormat("yyyy-MM-dd")
         gCal.setTime(f.parse(sDate))
@@ -25,14 +26,14 @@ object UtilDate {
     mDate
   }
 
-  def formatDate(gCal : GregorianCalendar): String = {
-    var rDate = gCal.get( Calendar.YEAR )  + "-"
-    rDate += addZero( gCal.get( Calendar.MONTH ) + 1 ) + "-"
-    rDate += addZero(gCal.get( Calendar.DATE )) + ""
+  def formatDate(gCal: GregorianCalendar): String = {
+    var rDate = gCal.get(Calendar.YEAR) + "-"
+    rDate += addZero(gCal.get(Calendar.MONTH) + 1) + "-"
+    rDate += addZero(gCal.get(Calendar.DATE)) + ""
     rDate
   }
 
-  def addZero(f:Int): String = {
+  def addZero(f: Int): String = {
     if (f < 10) "0" + f.toString else f.toString
   }
 }
