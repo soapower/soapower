e89638c294b62304bbd23fcd36a5b20f47d0520a
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2012-12-29 17:03:20 +0100

    Add index on isStats column

--- conf/evolutions/default/9.sql
+++ conf/evolutions/default/9.sql
@@ -8,7 +8,13 @@ alter table request_data alter column remoteTarget varchar(80) not null;
 
 update request_data set localTarget = '', remoteTarget = '' where purged = true;
 
+create index idx_request_data_5 on request_data (isStats);
+create index idx_request_data_6 on request_data (isStats, startTime);
+
 
 update environment set nbDayKeepAllData = 3;
 
 # --- !Downs
+
+drop index if exists idx_request_data_5;
+drop index if exists idx_request_data_6;
