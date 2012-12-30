b3621348e4008b1d7649966d7eb76c9179e3754f
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2012-12-30 14:12:45 +0100

    Activate scheduler

--- app/Global.scala
+++ app/Global.scala
@@ -16,11 +16,11 @@ object Global extends GlobalSettings {
 
     // initialDelay: Duration : 10 minutes
     // frequency: Duration : 10 hours
-    /*Akka.system.scheduler.schedule(10 minutes, 10 hours) {
+    Akka.system.scheduler.schedule(10 minutes, 10 hours) {
       Environment.compileStats()
       Environment.purgeXmlData()
       Environment.purgeAllData()
-    }*/
+    }
   }
 
   override def onStop(app: Application) {
