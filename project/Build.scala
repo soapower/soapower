296091d0077443a29553b7215554874aca844508
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2013-06-01 10:20:28 +0200

    Version stable 1.0.2

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
     val appName         = "soapower"
-    val appVersion      = "1.0.1"
+    val appVersion      = "1.0.2"
 
     val appDependencies = Seq(
         jdbc,
