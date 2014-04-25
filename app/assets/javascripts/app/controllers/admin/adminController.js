1d51eb21a92189d2d4de911cf239e9b9c31c0e84
guilefranc <guillaume.lefranc@sopra.com>
2014-04-25 15:20:08 +0200

    Begin work on Admin page : ad ui-select2 and datetimepicker to the form

--- app/assets/javascripts/app/controllers/admin/adminController.js
+++ app/assets/javascripts/app/controllers/admin/adminController.js
@@ -17,47 +17,53 @@ function AdminCtrl($scope, EnvironmentsService, $http, $filter, UIService) {
         $scope.showUploadRunning = false;
         $scope.showResponseUpload = true;
     };
+
     $scope.mindatecalendar = new Date();
     $scope.maxdatecalendar = new Date();
 
+    $scope.$watch('envir', function(newValue, oldValue) {
+           console.log("new : "+newValue+", old : "+oldValue);
+       });
+
      // Called when the maxdate datetimepicker is set
-     $scope.onMaxTimeSet = function (newDate, oldDate) {
-        $scope.showmaxdate = false;
-        $scope.maxdate = $filter('date')(newDate, "yyyy-MM-dd HH:mm");
-     };
+    $scope.onMaxTimeSet = function (newDate, oldDate) {
+       $scope.showmaxdate = false;
+       $scope.maxdate = $filter('date')(newDate, "yyyy-MM-dd HH:mm");
+    };
 
      // Called when the mindate datetimepicker is set
      $scope.onMinTimeSet = function (newDate, oldDate) {
         $scope.showmindate = false;
         $scope.mindate = $filter('date')(newDate, "yyyy-MM-dd HH:mm");
-        console.log($scope.mindatecalendar);
      };
 
     $scope.submitDelete = function () {
-        if (UIService.checkDatesFormatAndCompare($scope.mindate, $scope.maxdate)) {
-            // Dates are in correct format, perform the deletion
-            $scope.showRunningDelete = true;
-                    $scope.showResponseDelete = false;
-                    $scope.deleteForm.environmentName = $scope.deleteForm.environment.name;
-                    $scope.deleteForm.minDate = $scope.minDate;
-                    $scope.deleteForm.maxDate = $scope.maxDate;
 
-            $http.post('/admin/delete', $scope.deleteForm)
+        if (UIService.checkDatesFormatAndCompare($scope.mindate, $scope.maxdate)) {
+            UIService.reloadPage($scope, $scope.showServiceactions);
+        } else {
+            // Else, mindate and maxdate are set to yesterday's and today's dates
+            $scope.mindate = UIService.getInputCorrectDateFormat(UIService.getDay("yesterday"));
+            $scope.maxdate = UIService.getInputCorrectDateFormat(UIService.getDay("today"));
+        }
+
+        $scope.showRunningDelete = true;
+        $scope.showResponseDelete = false;
+        $scope.deleteForm.environmentName = $scope.envir;
+        $scope.deleteForm.minDate = $scope.minDate;
+        $scope.deleteForm.maxDate = $scope.maxDate;
+
+        $http.post('/admin/delete', $scope.deleteForm)
             .success(function (resp) {
                 $scope.responseDelete = resp;
                 $scope.showRunningDelete = false;
                 $scope.showResponseDelete = true;
-                })
+            })
             .error(function (resp) {
                 $scope.responseDelete = resp;
                 $scope.showRunningDelete = false;
                 $scope.showResponseDelete = true;
             });
-        } else {
-            // Else wrong format, mindate and maxdate are reset to yesterday's and today's dates
-            $scope.mindate = UIService.getInputCorrectDateFormat(UIService.getDay("yesterday"));
-            $scope.maxdate = UIService.getInputCorrectDateFormat(UIService.getDay("today"));
-        }
     };
 
     EnvironmentsService.findAllAndSelect($scope, null, 'all', null, true);
